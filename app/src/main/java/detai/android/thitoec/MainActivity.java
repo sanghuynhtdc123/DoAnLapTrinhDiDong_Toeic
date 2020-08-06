4aebe5bc8fb6bc664282fee917f10704697f3899
Sáng Huỳnh TDC <sanghuynhtdc123@users.noreply.github.com>
2020-08-06 16:06:49 +0700

    Code-Full

--- app/src/main/java/detai/android/thitoec/MainActivity.java
+++ app/src/main/java/detai/android/thitoec/MainActivity.java
@@ -9,6 +9,11 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button number_one;
+    private  Button number_two;
+    private Button number_three;
+    private Button number_four;
+    private Button number_five;
+    private  Button number_six;
 
 
 
@@ -17,11 +22,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         number_one = findViewById(R.id.btn_number_1);
+        number_two = findViewById(R.id.btn_number_2);
+        number_three = findViewById(R.id.btn_number_3);
+        number_four = findViewById(R.id.btn_number_4);
+        number_five = findViewById(R.id.btn_number_5);
+        number_six = findViewById(R.id.btn_number_6);
 
 
         number_one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TracNghiemToeic.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+        number_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TracNghiemToeic.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+        number_three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TracNghiemToeic.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+        number_four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TracNghiemToeic.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+        number_five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TracNghiemToeic.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+        number_six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, TracNghiemToeic.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                 startActivity(intent);
